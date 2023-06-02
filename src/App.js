b692d634a9ca89772e1f2f3be38c5f220dde667e
kappo <neupauer.dam@gmail.com>
2023-06-02 08:27:44 +0200

    first version

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import Configurator from "./pages/configurator";
+import ConfiguratorM from "./pages/configurator-mikro";
 import SearchEngine from "./pages/searchEngine";
+import ConfiguratorS from "./pages/configurator-s02";
+import Selectorrr from "./pages/selectorConfi";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Configurator />} />
+        <Route path="/" element={<Selectorrr />} />
+        <Route path="/mikro" element={<ConfiguratorM />} />
+        <Route path="/so2" element={<ConfiguratorS />} />
         <Route path="/search" element={<SearchEngine />} />
       </Routes>
     </BrowserRouter>
