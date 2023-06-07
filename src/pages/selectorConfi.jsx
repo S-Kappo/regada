226613c31e123ee5692a0e9a1b9e8e98e0a0c5da
kappo <neupauer.dam@gmail.com>
2023-06-07 07:26:39 +0200

    up

--- src/pages/selectorConfi.jsx
+++ src/pages/selectorConfi.jsx
@@ -1,27 +1,40 @@
 import { useNavigate } from "react-router-dom";
 
 const Selector = () => {
-    let navigate = useNavigate();
+  let navigate = useNavigate();
 
-    const naMikro = () => {
-        navigate("/mikro")
-    }
-    const naSO2 = () => {
-        navigate("/so2")
-    }
-    const naSearch = () => {
-        navigate("/search")
-    }
+  const naMikro = () => {
+    navigate("/mikro");
+    window.location.reload();
+  };
+  const naSO2 = () => {
+    navigate("/so2");
+    window.location.reload();
+  };
+  const naSearch = () => {
+    navigate("/search");
+    window.location.reload();
+  };
 
+  return (
+    <div className="buttony">
+      <button className="button-51" role="button" onClick={naMikro}>
+        SP MIRKO
+      </button>
+      <button className="button-51" role="button" onClick={naSO2}>
+        SO 2
+      </button>
+      <button className="button-51" role="button" onClick={naSearch}>
+        Dešifrovač
+      </button>
+      <a
+        href="https://www.figma.com/proto/mhMZPUW3voNaUdTw8oYhC5/Untitled?page-id=0%3A1&type=design&node-id=15-2&viewport=4471%2C-1205%2C1.71&scaling=scale-down&starting-point-node-id=15%3A2"
+        target="_blank"
+      >
+        Figma
+      </a>
+    </div>
+  );
+};
 
-    return (        
-        <div className="buttony">
-            <button className="button-51" role="button" onClick={naMikro}>SP MIRKO</button>
-            <button className="button-51" role="button"onClick={naSO2}>SO 2</button>
-            <button className="button-51" role="button" onClick={naSearch}>Dešifrovač</button>
-        </div>
-    )
-
-}
-
-export default Selector
+export default Selector;
